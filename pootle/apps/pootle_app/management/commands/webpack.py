f155fd6c77dabd2ccc4ccb898f266c77b19cee92
Igor Afanasyev <igor.afanasyev@gmail.com>
2016-09-28 20:24:07 +0000

    Webpack: do not hardcode trailing forward slash

--- pootle/apps/pootle_app/management/commands/webpack.py
+++ pootle/apps/pootle_app/management/commands/webpack.py
@@ -86,9 +86,9 @@ class Command(BaseCommand):
 
         if custom_static_dirs:
             # XXX: review this for css
-            # Append `js/` so that it's not necessary to reference it from the
+            # Append `js` so that it's not necessary to reference it from the
             # `webpack.config.js` file
-            custom_static_dirs = map(lambda x: os.path.join(x, 'js/'),
+            custom_static_dirs = map(lambda x: os.path.join(x, 'js'),
                                      custom_static_dirs)
             os.environ['WEBPACK_ROOT'] = ';'.join(custom_static_dirs)
 

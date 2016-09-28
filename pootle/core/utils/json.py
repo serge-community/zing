d0a972c6b9dbb5b2ab26b8a76ce88cf463eb9158
Igor Afanasyev <afan@evernote.com>
2016-09-28 12:49:15 -0700

    Sorting keys in the output allows for better gzip compression.
    On my quick test it gave 2% smaller gzip of the data structure.
    Might be a minor improvement, but it comes virtually for free.
    
    This will also give us a predictable output if we later want
    to compare serialized structures in tests.

--- pootle/core/utils/json.py
+++ pootle/core/utils/json.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) Pootle contributors.
+# Copyright (C) Zing contributors.
 #
-# This file is a part of the Pootle project. It is distributed under the GPL3
+# This file is a part of the Zing project. It is distributed under the GPL3
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
@@ -40,4 +41,4 @@ def jsonify(obj):
     else:
         indent = None
 
-    return json.dumps(obj, indent=indent, cls=PootleJSONEncoder)
+    return json.dumps(obj, indent=indent, cls=PootleJSONEncoder, sort_keys=True)

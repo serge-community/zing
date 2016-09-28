a2f0c56e08aade3a006caa046c065194c7577319
Igor Afanasyev <afan@evernote.com>
2016-09-28 15:27:18 -0700

    This helper function cleans up the dictionary
    Specifically, it removes default values that should better be
    dealt with on the client side (False and zero values, empty strings).
    Such a cleanup reduces the noise returned by the server and helps
    minimize the traffic.
    
    This commit is not strictly related to the browser table UI,
    but the underlying table view code already uses this feature,
    so I'm landing it here. It should be easy to remove this function
    and a couple of clean_dict() wrapper calls later if desired.

--- pootle/core/utils/json.py
+++ pootle/core/utils/json.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) Pootle contributors.
-# Copyright (C) Zing contributors.
 #
-# This file is a part of the Zing project. It is distributed under the GPL3
+# This file is a part of the Pootle project. It is distributed under the GPL3
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
@@ -42,3 +41,31 @@ def jsonify(obj):
         indent = None
 
     return json.dumps(obj, indent=indent, cls=PootleJSONEncoder, sort_keys=True)
+
+
+def clean_dict(obj):
+    """Cleans values that evaluate to false (empty strings
+    and zero values) since these can be easily dealt with
+    on the client side; this is a recursive function that
+    returns a new dictionary; it is expected for the dict
+    structure to only contain primitive types (no objects)"""
+    if isinstance(obj, dict):
+        out = dict()
+        for key in list(obj.keys()):
+            iout = clean_dict(obj[key])
+            if isinstance(obj[key], dict) and not len(iout.keys()):
+                continue
+            if isinstance(obj[key], list) and not len(iout):
+                continue
+            if iout:
+                out[key] = iout
+        return out
+    elif isinstance(obj, list):
+        out = list()
+        for item in obj:
+            iout = clean_dict(item)
+            if iout:
+                out.append(iout)
+        return out
+    else:
+        return obj

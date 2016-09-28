37d44a63c50abf9c57f539e8b5dba37f78e41d41
Igor Afanasyev <afan@evernote.com>
2016-09-28 13:08:30 -0700

    Use a cleaner way to encode native JSON object
    Instead of encoding JSON object as a string and wrapping it
    with JSON.parse() call, use a raw JSON string (which
    can be embedded into HTML page as is, but make sure
    we don't break HTML source parsing by having '</script>' text
    inside any JSON string.
    
    This makes resulting HTML+JS code more compact and readable.

--- pootle/core/templatetags/core.py
+++ pootle/core/templatetags/core.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) Pootle contributors.
+# Copyright (C) Zing contributors.
 #
-# This file is a part of the Pootle project. It is distributed under the GPL3
+# This file is a part of the Zing project. It is distributed under the GPL3
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
 from django import template
-from django.utils.html import escapejs
 from django.utils.safestring import mark_safe
 
 from ..utils.docs import get_docs_url
@@ -21,9 +21,10 @@ register = template.Library()
 @register.filter
 def to_js(value):
     """Returns a string which leaves the value readily available for JS
-    consumption.
+    consumption; breaks closing HTML tags into concatenated strings
+    to avoid '</script> inside JS string' problem
     """
-    return mark_safe('JSON.parse("%s")' % escapejs(jsonify(value)))
+    return mark_safe(jsonify(value).replace('</', '<\\/'))
 
 
 @register.filter

f4331a8ab742877fc70d92e56322518def33444f
Igor Afanasyev <igor.afanasyev@gmail.com>
2016-10-12 00:03:16 -0700

    Update project name in the header

--- pootle/core/utils/json.py
+++ pootle/core/utils/json.py
@@ -3,7 +3,7 @@
 # Copyright (C) Pootle contributors.
 # Copyright (C) Zing contributors.
 #
-# This file is a part of the Pootle project. It is distributed under the GPL3
+# This file is a part of the Zing project. It is distributed under the GPL3
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 

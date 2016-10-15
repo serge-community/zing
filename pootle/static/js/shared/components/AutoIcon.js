d83cb387732239e41630f664b910f3ece17ac8db
Igor Afanasyev <afan@evernote.com>
2016-10-14 22:59:06 -0700

    Add AutoIcon component
    This component can be used to provide fallback
    for avatars, and also for language/project icons
    in the future.

---
+++ pootle/static/js/shared/components/AutoIcon.js
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) Zing contributors.
+ *
+ * This file is a part of the Zing project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import React from 'react';
+
+import crc16 from '../utils/crc16'; // use relative path, otherwise tests will fail
+
+export function calcAbbreviation(title, length) {
+  const regexA = /\b[A-Z]/g;
+  const regexB = /\b[\w]/g;
+
+  let out = [];
+  let matches;
+  while ((matches = regexA.exec(title))) {
+    out.push(matches[0]);
+    if (out.length === length) {
+      return out.join('');
+    }
+  }
+
+  out = [];
+  while ((matches = regexB.exec(title))) {
+    out.push(matches[0]);
+    if (out.length === length) {
+      return out.join('').toUpperCase();
+    }
+  }
+
+  return title.substr(0, length).toUpperCase();
+}
+
+export function calcHue(title) {
+  return crc16(title) % 360;
+}
+
+class AutoIcon extends React.Component {
+
+  static propTypes() {
+    return {
+      title: React.PropTypes.string.isRequired,
+      saturation: React.PropTypes.number,
+      lightness: React.PropTypes.number,
+      abbreviationLength: React.PropTypes.number,
+      mode: React.PropTypes.string,
+      size: React.PropTypes.number,
+    };
+  }
+
+  constructor(props) {
+    super();
+
+    this.state = Object.assign({
+      saturation: 100,
+      lightness: 30,
+      abbreviationLength: 2,
+      mode: 'solid',
+      size: 20,
+    }, props);
+  }
+
+  componentDidMount() {
+    this.calculateProps(this.props.title);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.title !== newProps.title) {
+      this.calculateProps(newProps.title);
+    }
+  }
+
+  calculateProps(title) {
+    this.setState({
+      abbreviation: calcAbbreviation(title, this.state.abbreviationLength),
+      hue: calcHue(title),
+    });
+  }
+
+  render() {
+    const color =
+      `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`;
+
+    const size = `${this.state.size}px`;
+
+    const style = (this.state.mode === 'solid') ? {
+      backgroundColor: color,
+      border: `1px solid ${color}`,
+      color: '#fff',
+      width: size,
+      height: size,
+      lineHeight: size,
+    } : {
+      border: `1px solid ${color}`,
+      color,
+      width: size,
+      height: size,
+      lineHeight: size,
+    };
+
+    return (
+      <span
+        className="auto-icon"
+        style={style}
+      >{this.state.abbreviation}</span>
+    );
+  }
+
+}
+
+
+export default AutoIcon;

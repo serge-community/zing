4288e6ebd63261c5fac335c9f5593b20d50c5f91
Igor Afanasyev <afan@evernote.com>
2016-10-18 10:59:19 -0700

    Fix rendering: ModalFooter wrapper would be used twice

--- pootle/static/js/shared/components/Dialog.js
+++ pootle/static/js/shared/components/Dialog.js
@@ -1,14 +1,15 @@
 /*
  * Copyright (C) Pootle contributors.
+ * Copyright (C) Zing contributors.
  *
- * This file is a part of the Pootle project. It is distributed under the GPL3
+ * This file is a part of the Zing project. It is distributed under the GPL3
  * or later license. See the LICENSE file for a copy of the license and the
  * AUTHORS file for copyright and authorship information.
  */
 
 import React from 'react';
 
-import Modal, { ModalFooter } from './Modal';
+import Modal from './Modal';
 
 
 const Dialog = React.createClass({
@@ -37,7 +38,7 @@ const Dialog = React.createClass({
 
   renderFooter() {
     return (
-      <ModalFooter>
+      <div>
         <button
           className="btn btn-primary"
           onClick={this.props.onAccept}
@@ -51,7 +52,7 @@ const Dialog = React.createClass({
         >
           {this.props.cancelLabel}
         </button>
-      </ModalFooter>
+      </div>
     );
   },
 

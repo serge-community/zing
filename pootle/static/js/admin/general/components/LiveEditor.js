c6516ab00dcb7250d05992f3180138e979a5e133
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-11-08 14:41:51 +0100

    LiveEditor: preview requires a POST request

--- pootle/static/js/admin/general/components/LiveEditor.js
+++ pootle/static/js/admin/general/components/LiveEditor.js
@@ -1,7 +1,8 @@
 /*
  * Copyright (C) Pootle contributors.
+ * Copyright (C) Zing contributors.
  *
- * This file is a part of the Pootle project. It is distributed under the GPL3
+ * This file is a part of the Zing project. It is distributed under the GPL3
  * or later license. See the LICENSE file for a copy of the license and the
  * AUTHORS file for copyright and authorship information.
  */
@@ -95,6 +96,7 @@ export const LiveEditor = React.createClass({
   loadRemotePreview() {
     fetch({
       url: '/xhr/preview/',
+      method: 'POST',
       body: {
         text: this.state.value,
       },

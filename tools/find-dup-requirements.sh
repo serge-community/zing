7ecd5155de197449afedfa88fba053de438031cc
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-11-12 13:52:46 +0100

    Cleanup: remove unused/obsolete files

--- tools/find-dup-requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dups=$(cat requirements/*.txt | egrep -v "^#|^$|^-r" | cut -d">" -f1 | cut -d "<" -f1 | cut -d'!' -f1 | cut -d"=" -f1 | sort | uniq -d)
-
-for dup in $dups
-do
-    egrep $dup requirements/*.txt
-done

ea31e76c56e004d22d85d6bf4b85ac31d7ec1c39
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-11-23 13:21:57 +0100

    Autosize: upgrade to 3.0.18
    Likewise it allows us to remove a workaround in the `AutosizeTextarea`
    component.

--- pootle/static/js/shared/components/AutosizeTextarea.js
+++ pootle/static/js/shared/components/AutosizeTextarea.js
@@ -1,7 +1,8 @@
 /*
  * Copyright (C) Pootle contributors.
+ * Copyright (C) Zing contributors.
  *
- * This file is a part of the Pootle project. It is distributed under the GPL3
+ * This file is a part of the Zing project. It is distributed under the GPL3
  * or later license. See the LICENSE file for a copy of the license and the
  * AUTHORS file for copyright and authorship information.
  */
@@ -21,21 +22,7 @@ const AutosizeTextarea = React.createClass({
   },
 
   componentWillUnmount() {
-    // Seems we're hitting the bug in Firefox under Windows:
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=889376
-    // (autosize.js tries to run the cleanup by dispatching
-    // an event, and if our dom node is detached,
-    // Firefox throws the NS_ERROR_UNEXPECTED error).
-    // Until this is fixed in Autosize, we wrap the call to
-    // autosize.destroy() with try..catch
-    try {
-      autosize.destroy(this.refs.textarea);
-    } catch (err) {
-      if (err.name === 'NS_ERROR_UNEXPECTED') {
-        return;
-      }
-      throw err;
-    }
+    autosize.destroy(this.refs.textarea);
   },
 
   render() {

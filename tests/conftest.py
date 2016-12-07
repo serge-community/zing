db1c21cb427a10af97e9b936da9e0b61f8912e7a
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-12-07 15:52:23 +0100

    Tests: refactor tests directory into a fixture helper

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@
 
 import os
 
+import pytest
+
 from django import setup
 from django.conf import settings
 
@@ -15,15 +17,21 @@ import logging
 
 logging.getLogger("factory").setLevel(logging.WARN)
 
+WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+@pytest.fixture
+def tests_dir():
+    """Helper function to join a path relative to the tests directory."""
+    return lambda path: os.path.join(WORKING_DIR, path)
+
 
 def pytest_configure():
     if not settings.configured:
         from pootle import syspath_override  # Needed for monkey-patching
         syspath_override
         os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
-        WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
-        os.environ['POOTLE_SETTINGS'] = os.path.join(WORKING_DIR,
-                                                     'settings.py')
+        os.environ['POOTLE_SETTINGS'] = os.path.join(WORKING_DIR, 'settings.py')
         # The call to `setup()` was needed before a fix for
         # pytest-dev/pytest-django#146 was available. This happened in version
         # 2.9; unfortunately upgrading to 2.9+ is not possible yet because a fix

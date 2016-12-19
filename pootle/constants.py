a04a286d157acb725d7fa17f4749dd72d249c58f
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-12-19 13:49:04 +0100

    Version: bump up

--- pootle/constants.py
+++ pootle/constants.py
@@ -7,4 +7,4 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-VERSION = (0, 1, 4, 'final', 0)
+VERSION = (0, 1, 5, 'beta', 0)

a95b8a86ec4fa8a9dac162c714a0a14d7e054a56
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-01-20 15:19:05 +0100

    Due dates: implement basic backend
    This commit adds a model to store due dates, as well as an API view to create,
    update and delete them.

--- pootle/models/__init__.py
+++ pootle/models/__init__.py
@@ -6,7 +6,8 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from .duedate import DueDate
 from .stats import Stats
 
 
-__all__ = ('Stats',)
+__all__ = ('DueDate', 'Stats')

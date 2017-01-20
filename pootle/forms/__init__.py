632982d5e1cc1f18265f501c244f68c1d1103f22
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-01-20 15:19:05 +0100

    Due dates: implement basic backend
    This commit adds a model to store due dates, as well as an API view to create,
    update and delete them.

---
+++ pootle/forms/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Zing contributors.
+#
+# This file is a part of the Zing project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from .duedate import DueDateForm
+
+
+__all__ = ('DueDateForm',)
---
+++ pootle/forms/duedate.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Zing contributors.
+#
+# This file is a part of the Zing project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django import forms
+
+from pootle.forms.fields import UnixTimestampField
+from pootle.models import DueDate
+
+
+class DueDateForm(forms.ModelForm):
+
+    due_on = UnixTimestampField()
+
+    class Meta:
+        model = DueDate
+        fields = ('due_on', 'pootle_path', 'modified_by', )

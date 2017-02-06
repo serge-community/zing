a77c6176471fa06bac724c54f00d068fe1e13a10
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-02-06 16:38:17 +0100

    DueDateForm: require less fields in edit form
    This allows to update due dates without passing redundant data.

--- pootle/forms/__init__.py
+++ pootle/forms/__init__.py
@@ -6,7 +6,7 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from .duedate import DueDateForm
+from .duedate import AddDueDateForm, EditDueDateForm
 
 
-__all__ = ('DueDateForm',)
+__all__ = ('AddDueDateForm', 'EditDueDateForm', )

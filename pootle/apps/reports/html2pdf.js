40f9b8154aaaa3fa31a30e6e157eac60bdf581aa
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-02-09 10:59:48 +0100

    Adjust copyright notices

--- pootle/apps/reports/html2pdf.js
+++ pootle/apps/reports/html2pdf.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) Zing contributors.
+ *
+ * This file is a part of the Zing project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
 var system = require('system')
 var page = require('webpage').create();
 var fs = require('fs');

59d35db1eed9edad46fb77abd8bd5bdf3519041d
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-02-21 14:26:38 +0100

    Due dates: propagate project changes to TPs
    When creating/updating/deleting a due date at the project level, the
    operation will affect all existing TPs that belong to the project, which
    means that these will take over any language-specific due dates that
    might have existed before.
    
    This change will also considerably improve querying due dates for
    particular languages.

--- tests/models/duedate.py
+++ tests/models/duedate.py
@@ -9,11 +9,12 @@
 import pytest
 
 from django.core.exceptions import ValidationError
-from django.utils import timezone
 
-from pytest_pootle.factories import DueDateFactory
+from pytest_pootle.factories import DueDateFactory, UserFactory
 
+from pootle.core.utils.timezone import aware_datetime
 from pootle.models import DueDate
+from pootle_translationproject.models import TranslationProject
 
 
 @pytest.mark.django_db
@@ -47,3 +48,73 @@ def test_duedate_create(pootle_path):
         pootle_path=pootle_path,
     )
     assert due_date.id is not None
+
+
+@pytest.mark.django_db
+def test_duedate_create_project_propagates():
+    """Tests due date creation at the project level propagates to
+    existing TPs.
+    """
+    project_code = 'project0'
+    pootle_path = '/projects/%s/' % project_code
+    DueDateFactory.create(
+        pootle_path=pootle_path,
+    )
+
+    tp_due_dates = DueDate.objects.for_project(project_code).exclude(
+        pootle_path__startswith=pootle_path,
+    )
+    tps = TranslationProject.objects.filter(project__code=project_code)
+    assert tp_due_dates.count() == tps.count() != 0
+
+
+@pytest.mark.django_db
+def test_duedate_update_project_propagates():
+    """Tests due date updates at the project level propagates to
+    existing TP due dates.
+    """
+    project_code = 'project0'
+    pootle_path = '/projects/%s/' % project_code
+    due_date = DueDateFactory.create(
+        pootle_path=pootle_path,
+    )
+
+    # Update due date with different values
+    other_user = UserFactory.create()
+    other_due_on = aware_datetime(2017, 1, 1, 1, 2, 3)
+    due_date.due_on = other_due_on
+    due_date.modified_by = other_user
+    due_date.save()
+
+    tp_due_dates = DueDate.objects.for_project(project_code).exclude(
+        pootle_path__startswith=pootle_path,
+    )
+    for tp_due_date in tp_due_dates:
+        assert tp_due_date.due_on == other_due_on
+        assert tp_due_date.modified_by == other_user
+
+
+@pytest.mark.django_db
+def test_duedate_delete_project_propagates():
+    """Tests due date deletion at the project level propagates to
+    existing TP due dates.
+    """
+    project_code = 'project0'
+    pootle_path = '/projects/%s/' % project_code
+    due_date = DueDateFactory.create(
+        pootle_path=pootle_path,
+    )
+
+    tp_due_dates = DueDate.objects.for_project(project_code).exclude(
+        pootle_path__startswith=pootle_path,
+    )
+    tps = TranslationProject.objects.filter(project__code=project_code)
+    assert tp_due_dates.count() == tps.count() != 0
+
+    due_date.delete()
+
+    tp_due_dates = DueDate.objects.for_project(project_code).exclude(
+        pootle_path__startswith=pootle_path,
+    )
+
+    assert tp_due_dates.count() == 0

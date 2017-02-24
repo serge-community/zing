20411d0758690a2467d6a97cbeaffbe01b6e14c5
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-02-24 13:21:27 +0100

    URL utils: implement internal path split utility
    This is a port of the backend's path splitting function so we can use it
    to easily and reliably split internal paths into manageable parts, which
    can then be leveraged for path and URL generation purposes.

--- pootle/static/js/shared/utils/url.js
+++ pootle/static/js/shared/utils/url.js
@@ -6,6 +6,57 @@
  * AUTHORS file for copyright and authorship information.
  */
 
+/**
+ * Split a string while returning the remainder.
+ *
+ * Note JS's `String.prototype.split` method unfortunately doesn't
+ * return the remainder in the last index in case a limit parameter
+ * was specified.
+ */
+export function split(value, separator, limit) {
+  const parts = value.split(separator);
+
+  if (parts.length <= limit) {
+    return parts;
+  }
+  return parts.splice(0, limit).concat(parts.join(separator));
+}
+
+
+/**
+ * Splits an internal path into language, project, directory and file parts.
+ * @param {string} path - internal path
+ * @return {array} - [lang_code, proj_code, dir_path, filename]
+ */
+export function splitPootlePath(path) {
+  const slashCount = path.split('/').length - 1;
+  const parts = split(path, '/', 3).slice(1);
+
+  let languageCode = null;
+  let projectCode = null;
+  let ctx = '';
+
+  if (slashCount !== 0 && path !== '/projects/') {
+    if (slashCount === 2) {
+      languageCode = parts[0];
+    // FIXME: use `.startsWith()` after dropping IE11 support
+    } else if (/^\/projects\//.test(path)) {
+      projectCode = parts[1];
+      ctx = parts[2];
+    } else if (slashCount !== 1) {
+      languageCode = parts[0];
+      projectCode = parts[1];
+      ctx = parts[2];
+    }
+  }
+
+  const lastSlash = ctx.lastIndexOf('/');
+  const dirPath = ctx.substring(0, lastSlash + 1);
+  const filename = ctx.substring(lastSlash + 1, ctx.length);
+
+  return [languageCode, projectCode, dirPath, filename];
+}
+
 
 /**
  * Retrieves a translation URL out of an internal `path`

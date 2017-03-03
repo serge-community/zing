13b2fe385f39a67c10b43012065cf108298629a8
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-03-03 13:35:57 +0100

    Tasks: improve display of dates
    Dates will now display the month name and the day, and the tooltip will
    include the full date and time in the host computer's timezone.

--- pootle/static/js/setup.test.js
+++ pootle/static/js/setup.test.js
@@ -1,22 +1,23 @@
 /*
- * Copyright (C) Pootle contributors.
+ * Copyright (C) Zing contributors.
  *
- * This file is a part of the Pootle project. It is distributed under the GPL3
+ * This file is a part of the Zing project. It is distributed under the GPL3
  * or later license. See the LICENSE file for a copy of the license and the
  * AUTHORS file for copyright and authorship information.
  */
 
-import { before } from 'mocha';
+global.gettext = string => string;
 
+global.ngettext = (singular, plural, count) => (
+  (count === 1) ? singular : plural
+);
 
-before(() => {
-  global.gettext = string => string;
+global.interpolate = (string, args) => (
+  string.replace(/%\(\w+\)s/g, (match) => String(args[match.slice(2, -2)]))
+);
 
-  global.ngettext = (singular, plural, count) => (
-    (count === 1) ? singular : plural
-  );
-
-  global.interpolate = (string, args) => (
-    string.replace(/%\(\w+\)s/g, (match) => String(args[match.slice(2, -2)]))
-  );
-});
+global.PTL = {
+  settings: {
+    UI_LOCALE: 'en',
+  },
+};

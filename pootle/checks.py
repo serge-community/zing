36c7790ba4de34f4b276f7ad9d0f6a66c4dbc4c4
Julen Ruiz Aizpuru <julenx@gmail.com>
2017-12-09 11:49:41 +0100

    Req: use Django 1.11

--- pootle/checks.py
+++ pootle/checks.py
@@ -17,7 +17,7 @@ from pootle.i18n.gettext import ugettext as _
 TTK_MINIMUM_REQUIRED_VERSION = (2, 2, 5)
 
 # Minimum Django version required for Pootle to run.
-DJANGO_MINIMUM_REQUIRED_VERSION = (1, 10, 8)
+DJANGO_MINIMUM_REQUIRED_VERSION = (1, 11, 8)
 
 # Minimum lxml version required for Pootle to run.
 LXML_MINIMUM_REQUIRED_VERSION = (3, 0, 0, 0)

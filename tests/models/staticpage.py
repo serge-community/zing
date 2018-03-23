76345c23c9e60cb116744e82ada55c327b931489
Julen Ruiz Aizpuru <julenx@gmail.com>
2018-03-23 16:33:58 +0100

    StaticPages: add failing test for timestamp update

--- tests/models/staticpage.py
+++ tests/models/staticpage.py
@@ -6,9 +6,26 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import pytest
+
 from pytest_pootle.factories import StaticPageFactory
 
 
 def test_staticpage_repr():
     staticpage = StaticPageFactory.build(virtual_path='/test/')
     assert '<StaticPage: /test/>' == repr(staticpage)
+
+
+@pytest.mark.django_db
+def test_staticpage_save_with_changes():
+    """Tests a static page's timestamp is updated upon saving changes (#316)."""
+    page = StaticPageFactory.build(virtual_path='/test/', title='foo')
+    page.save()
+
+    page.title = 'bar'
+    assert page.has_changes()
+
+    old_mtime = page.modified_on
+    page.save()
+
+    assert page.modified_on != old_mtime

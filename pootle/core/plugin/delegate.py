0adba68db7658d5e2769f315f482f416639a7cb6
Julen Ruiz Aizpuru <julenx@gmail.com>
2018-03-27 12:43:47 +0200

    Cleanup: remove dead code

--- pootle/core/plugin/delegate.py
+++ pootle/core/plugin/delegate.py
@@ -9,50 +9,6 @@
 from django.dispatch import Signal
 from django.dispatch.dispatcher import NO_RECEIVERS
 
-from .exceptions import StopProviding
-from .results import GatheredDict
-
-
-class Provider(Signal):
-
-    result_class = GatheredDict
-
-    def __init__(self, *args, **kwargs):
-        self.result_class = kwargs.pop("result_class", self.result_class)
-        super(Provider, self).__init__(*args, **kwargs)
-
-    def gather(self, sender=None, **named):
-        gathered = self.result_class(self)
-        named["gathered"] = gathered
-        no_receivers = (
-            not self.receivers
-            or self.sender_receivers_cache.get(sender) is NO_RECEIVERS)
-        if no_receivers:
-            return gathered
-        for provider in self._live_receivers(sender):
-            try:
-                gathered.add_result(
-                    provider,
-                    provider(signal=self, sender=sender, **named))
-            except StopProviding as e:
-                # allow a provider to prevent further gathering
-                gathered.add_result(
-                    provider,
-                    e.result)
-                break
-        return gathered
-
-
-def provider(signal, **kwargs):
-    def _decorator(func):
-        if isinstance(signal, (list, tuple)):
-            for s in signal:
-                s.connect(func, **kwargs)
-        else:
-            signal.connect(func, **kwargs)
-        return func
-    return _decorator
-
 
 class Getter(Signal):
 

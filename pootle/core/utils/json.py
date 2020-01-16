415c2b3ab149e21268b77400fab567417c5c0d18
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-01-16 13:04:05 +0100

    py3: absolute_import is now default behavior

--- pootle/core/utils/json.py
+++ pootle/core/utils/json.py
@@ -7,7 +7,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from __future__ import absolute_import
 
 import datetime
 import json

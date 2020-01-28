84e69986bf37e4a44f699368662b57b31a69610f
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-01-28 14:14:22 +0100

    py3: fix DeprecationWarning for import

--- pootle/core/utils/list.py
+++ pootle/core/utils/list.py
@@ -6,7 +6,7 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from collections import Iterable
+from collections.abc import Iterable
 
 
 def flatten(elements):

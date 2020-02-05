954c7b8b7add2b99025400cee6f2d3bc3e92b558
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-02-05 08:31:12 +0100

    StatsForm: offset must always be positive

--- pootle/apps/pootle_statistics/forms.py
+++ pootle/apps/pootle_statistics/forms.py
@@ -14,7 +14,7 @@ from pootle.core.forms import PathForm
 
 class StatsForm(PathForm):
 
-    offset = forms.IntegerField(required=False)
+    offset = forms.IntegerField(required=False, min_value=0)
 
     def clean_offset(self):
         return self.cleaned_data.get("offset", 0)

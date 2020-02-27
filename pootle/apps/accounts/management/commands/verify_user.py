f682d77105e987ce5edd6242be92aea39124a6b0
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-02-27 15:29:29 +0100

    py3: e.message doesn't exist

--- pootle/apps/accounts/management/commands/verify_user.py
+++ pootle/apps/accounts/management/commands/verify_user.py
@@ -45,7 +45,7 @@ class Command(UserCommand):
                     utils.verify_user(user)
                     self.stdout.write("Verified user '%s'" % user.username)
                 except (ValueError, ValidationError) as e:
-                    self.stderr.write(e.message)
+                    self.stderr.write(str(e))
 
         if options["user"]:
             for user in options["user"]:
@@ -53,4 +53,4 @@ class Command(UserCommand):
                     utils.verify_user(self.get_user(user))
                     self.stdout.write("User '%s' has been verified" % user)
                 except (ValueError, ValidationError) as e:
-                    self.stderr.write(e.message)
+                    self.stderr.write(str(e))

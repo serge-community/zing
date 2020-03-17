d7326a312f2d2db6a3faeb2f248961748b3a1ec1
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-03-17 19:38:06 +0100

    Cleanup: remove superfluous fixture usage

--- tests/core/mixins/treeitem.py
+++ tests/core/mixins/treeitem.py
@@ -101,7 +101,7 @@ def test_get_children(project0, language0):
 
 
 @pytest.mark.django_db
-def test_get_parent(po_directory, project0, language0, tp0, store0, subdir0):
+def test_get_parent(project0, language0, tp0, store0, subdir0):
     """Ensure that retrieved parent objects have a correct type."""
 
     subdir_store = subdir0.child_stores.first()

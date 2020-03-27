f5a5e31f083af4452120d0f2c713aa1760ed94f8
Julen Ruiz Aizpuru <julenx@gmail.com>
2020-03-27 12:10:11 +0100

    ESLint: switch to basic config
    We don't need a fine-grained config like Airbnb's, mostly because
    formatting matters will be addressed via Prettier.

--- pootle/static/js/.eslintrc.js
+++ pootle/static/js/.eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  'extends': 'airbnb',
   env: {
     browser: true,
   },
@@ -22,10 +21,22 @@ module.exports = {
     'react',
   ],
   rules: {
+    'curly': 'error',
+    'no-cond-assign': [2, 'except-parens'],
+    'no-console': 'error',
+    'no-constant-condition': 'error',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    'no-extra-semi': 'error',
+    'no-redeclare': 'error',
+    'no-self-assign': 'error',
+    'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-unused-vars': 'error',
+    'no-useless-escape': 'error',
+
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
     'react/prefer-es6-class': 0,
     'react/prefer-stateless-function': 1,
-
-    // see http://blog.javascripting.com/2015/09/07/fine-tuning-airbnbs-eslint-config/
-    'no-cond-assign': [2, 'except-parens'],
   },
 };
